0066e5233734f4e48bc86f3e74fa45301e69a432
Ayush Vaibhav Bhatti <154561306+ayushbhatti@users.noreply.github.com>
2025-12-10 14:14:12 -0700

    deleted extra files

--- Models/Linear_head.py
+++
@@ -1,9 +0,0 @@
-# Simple Trainable Classifiaction Head
-import torch.nn as nn
-
-class LinearHead(nn.Module):
-    def __init__(self, in_dim, num_classes):
-        super().__init__()
-        self.fc = nn.Linear(in_dim, num_classes)
-    def forward(self, x):
-        return self.fc(x)
